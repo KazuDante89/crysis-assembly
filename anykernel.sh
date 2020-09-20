1d87ece9c98801b47f793377ab210191a439fbd4
KazuDante89 <35647262+KazuDante89@users.noreply.github.com>
2020-09-20 17:08:37 -0400

    Add files via upload

---
+++ anykernel.sh
@@ -0,0 +1,31 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Crysis-Kernel by @KazuDante
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=lavender
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=none;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+write_boot;
+## end install
